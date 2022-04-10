3f56ef85e47acc3ce0ea05143262b5bb73eb4784
Mahima Mandhana <mandhana.mahima@gmail.com>
2022-04-10 14:59:31 -0700

    Updates

---
+++ precache-manifest.d212a40695be5e73a38e35b71f895fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b36759ec36c02f6ddd72",
+    "url": "/HW3/static/css/main.737d37e1.chunk.css"
+  },
+  {
+    "revision": "b36759ec36c02f6ddd72",
+    "url": "/HW3/static/js/main.b36759ec.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/HW3/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "dca53c1e5864bdaa354a",
+    "url": "/HW3/static/js/2.dca53c1e.chunk.js"
+  },
+  {
+    "revision": "6cc19e45a5de96ec1605",
+    "url": "/HW3/static/js/runtime~main.6cc19e45.js"
+  },
+  {
+    "revision": "69c76017def956a8185f8fef4b8f2beb",
+    "url": "/HW3/index.html"
+  }
+];
