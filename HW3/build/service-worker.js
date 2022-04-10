3e353b02ee0723c58ad9c30de25117813c6a38b7
Mahima Mandhana <mandhana.mahima@gmail.com>
2022-04-09 18:41:46 -0700

    remove latest commits

--- HW3/build/service-worker.js
+++ HW3/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b0df2b5d228e11b63fcca32a70d401d.js"
+  "/HW3/build/precache-manifest.d664e71b93462b6257aa27fc7ba85546.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/HW3/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
--- HW3/src/App.js
+++ HW3/src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
 import Campus from './Campus'; 
+import React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Map} from '@esri/react-arcgis';
-import {Scene} from '@esri/react-arcgis';
-import {WebMap,WebScene} from '@esri/react-arcgis';
 
 function App() {
-	ReactDOM.render(
-	  <WebMap style={{width:'80vw',height:'80vh',border:'1px solid black',margin:'10px'}} id="0cdc7786dbb4485694ad88dd8a46d39b" /> ,
-	  document.getElementById('container')
-	);// ReactDOM.render()
+ ReactDOM.render(
+  <Map /> ,
+  document.getElementById('container')
+ );// ReactDOM.render()
 }// App()
 
 export default (props) => (
     <Map style={{ width: '70vw', height: '90vh' }}
-        //mapProperties={{ basemap: 'satellite' }}
         viewProperties={{
-            center: [-118.29,34.03],
-            zoom: 10
+            center: [-118.28538,34.025],
+            zoom: 14
         }}>
         <Campus />
     </Map>
